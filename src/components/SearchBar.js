aa5be6f7347335b95a66d42ac45909679ea3d79d
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-01 22:02:04 +0530

    Added event handlers to the SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,19 @@ import React from 'react';
 class SearchBar extends React.Component {
 	state = { term: '' };
 
-	onInputChange = () => {};
+	onInputChange = (event) => {
+		//Collect the value entered by user in the search bar
+		this.setState({ term: event.target.value });
+	};
 
+	onFormSubmit = (event) => {
+		//Stop the form from submitting as soon as the user hits enter
+		event.preventDefault();
+	};
 	render() {
 		return (
 			<div className="search-bar ui segment">
-				<form className="ui form">
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>Video Search</label>
 						<input
