684753ba2ac8e23bfc8c246747a7844351b6af43
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-02 11:15:26 +0530

    Added styling

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,7 +4,24 @@ const VideoDetail = ({ video }) => {
 	if (!video) {
 		return <div>Loading...</div>;
 	}
-	return <div>{video.snippet.title}</div>;
+	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+	return (
+		<div>
+			<div className="ui segment">
+				<iframe
+					src={videoSrc}
+					title={video.id.videoId}
+					allow="fullscreen;"
+					width="100%"
+					height="410"
+				/>
+			</div>
+			<div className="ui segment">
+				<h4 className="ui header">{video.snippet.title}</h4>
+				<p>{video.snippet.description}</p>
+			</div>
+		</div>
+	);
 };
 
 export default VideoDetail;
